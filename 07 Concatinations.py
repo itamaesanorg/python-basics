e293c20b3f5ff93af24b90c09f579c898b35b97e
miguelgargallo <mgargallo16@gmail.com>
2022-08-21 23:22:51 +0200

    1.0.7

--- 07 Concatinations.py
+++ 07 Concatinations.py
@@ -10,12 +10,13 @@ name = input("Waht is your name? \n")
 
 print("Hello " + name+"," + " Thank you so much for coming to our shop. \n\n")
 
-print("""We have the following drinks:\n
-Late $2.99\n
-Hot $1.99\n
-Cold $3.99\n""")
+print("""We have the following drinks:
+Late $2.99
+Hot $1.99
+Cold $3.99\n\n""")
 
-coffee = input("What do you wish for this sunny day? \n")
+coffee = input("What do you wish for this sunny day? \n\n")
 
-print("Okay please, wait a second, I will take your " + coffee + " coffe inmediatly.")
+print("Okay please, wait a second, I will take your " + coffee + " coffe inmediatly.\n")
 
+ 
