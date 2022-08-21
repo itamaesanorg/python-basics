6171d17c116816b696d50e0eddbd1feb546f4207
miguelgargallo <mgargallo16@gmail.com>
2022-08-21 23:21:10 +0200

    1.0.7

--- 07 Concatinations.py
+++ 07 Concatinations.py
@@ -8,15 +8,14 @@ print("Hello, and welcome to the ITAMEASAN Cofe shop!!!!!!")
 
 name = input("Waht is your name? \n")
 
-print("Hello " + name+"," + " Thank you so much for coming to our shop.")
+print("Hello " + name+"," + " Thank you so much for coming to our shop. \n\n")
 
-print("""We have the following drinks:
-Late $2.99
-Hot $1.99
-Cold $3.99""")
+print("""We have the following drinks:\n
+Late $2.99\n
+Hot $1.99\n
+Cold $3.99\n""")
 
 coffee = input("What do you wish for this sunny day? \n")
 
 print("Okay please, wait a second, I will take your " + coffee + " coffe inmediatly.")
 
- 
