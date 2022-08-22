605ed5192e89da3077ece133c054c47bfc9afbd8
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 17:42:37 +0200

    1.18.0

--- 17 Evaluate.py
+++ 17 Evaluate.py
@@ -1,5 +1,9 @@
 # Lets work with the data
 
-if 4 > 3:
+if 2 > 3:
     print("yep, its trueeeee")
     print("yess, its also true")
+else:
+    print("nope, its false")
+    
+# In pyhthon we use, <, >, => or <= and == to compare values.
