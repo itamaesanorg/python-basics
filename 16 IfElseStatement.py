4291c9c6e8736552821603a79008608a80b6ca90
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 17:31:33 +0200

    1.16.0

--- 16 IfElseStatement.py
+++ 16 IfElseStatement.py
@@ -1,2 +1,14 @@
 # We are going to discover If and Else statements in this program too.
 
+print("Welcome to the coffee shop!")
+
+name = input("What is your name? \n")
+
+if name == "Ben":
+    print("I don't like you, " + name + "\n \n Ben quits the coffee shop.")
+else:
+    print("Hello " + name + ", welcome to the coffee shop.")
+    print("We have the following drinks:")
+    print("Late $2.99")
+    print("Hot $1.99")
+    print("Cold $3.99")
