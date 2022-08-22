6991d8ab82378bc5713fb5eb11dd115be45196d8
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 17:35:41 +0200

    1.16.0

--- 16 IfElseStatement.py
+++ 16 IfElseStatement.py
@@ -7,8 +7,8 @@ name = input("What is your name? \n")
 if name == "Ben":
     print("I don't like you, " + name + "\n \n Ben quits the coffee shop.")
 else:
-    print("Hello " + name + ", welcome to the coffee shop.")
+    print("Hello " + name + ", welcome to the coffee shop. \n")
     print("We have the following drinks:")
-    print("Late $2.99")
-    print("Hot $1.99")
-    print("Cold $3.99")
+    print("""Late $2.99
+Hot $1.99
+Cold $3.99""")
