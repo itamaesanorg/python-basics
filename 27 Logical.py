6aa823505f6c49ada2f26b696658f4ae5288e91a
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 19:55:28 +0200

    1.27.0 Logic

---
+++ 27 Logical.py
@@ -0,0 +1,22 @@
+
+print("Hello, welcome to ITAMAESAN Coffee!!!!!!!!!")
+
+name = input("What is your name?\n")
+
+if name == "Ben":
+    evil_status = input("Are you evil?\n")
+    if evil_status == "Yes":
+        print("You're not welcome here Ben! Get out of here!")
+        exit()
+    else:
+        print("Oh! You are one of those good Bens! Come in! \n\n\n")
+else:
+    print("Hello " + name + ", welcome to the coffee shop. \n\n\n")
+    
+beard_length = int(input("How long is your beard?\n"))
+
+if beard_length >= 1:
+    print("*oh, hello good sir, nice beard. You may go to the front of the line.")
+else:
+    print("*oh, hello good sir, nice beard. You may go to the back of the line.")
+
