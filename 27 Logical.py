a13b1825bf6449ae5696e8e3a570f3420ab54666
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 19:57:57 +0200

    1.27.0 Logic

--- 27 Logical.py
+++ 27 Logical.py
@@ -6,17 +6,17 @@ name = input("What is your name?\n")
 if name == "Ben" or name == "Patricia":
     evil_status = input("Are you evil?\n")
     if evil_status == "Yes":
-        print("You're not welcome here Ben! Get out of here!")
+        print("You're not welcome here " + name + " Get out of here!")
         exit()
     else:
-        print("Oh! You are one of those good" + name + "\n Come in! \n\n\n")
+        print("Oh! You are one of those good " + name + "\n Come in! \n\n\n")
 else:
     print("Hello " + name + ", welcome to the coffee shop. \n\n\n")
     
 beard_length = int(input("How long is your beard?\n"))
 
 if beard_length >= 1:
-    print("*oh, hello good sir, nice beard. You may go to the front of the line.")
+    print("*oh, hello good, nice beard. You may go to the front of the line.")
 else:
-    print("*oh, hello good sir, nice beard. You may go to the back of the line.")
+    print("*oh, hello good, nice beard. You may go to the back of the line.")
 
