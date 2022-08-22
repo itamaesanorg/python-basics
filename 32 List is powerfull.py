f4a3128958655ecd74de74a42d25f955b220be28
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 20:51:23 +0200

    1.32.0 List

---
+++ 32 List is powerfull.py
@@ -0,0 +1,5 @@
+# Hey we are going on a trip to the beach.
+
+# Here are some things you will need to carry with you:
+
+# towel, sunblock, sunscreen, hat, sunglasses, swimsuit, iPhone, homekeys, wallet, ehternet cable, gaming laptop, camera, gameboy-color, batteries, water battle, time capsule, marshmallows, beer, wine, snacks, burguers.
