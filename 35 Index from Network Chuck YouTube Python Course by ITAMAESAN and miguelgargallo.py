ff1c98a65d9d4c63943021ad3ca84bce6b31a9c4
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 21:30:09 +0200

    Network Chuck YouTube Python Course by ITAMAESAN

---
+++ 35 Index from Network Chuck YouTube Python Course by ITAMAESAN and miguelgargallo.py
@@ -0,0 +1,29 @@
+# Hey we are going on a trip to the Camp Beach.
+
+# Here are some things you will need to carry with you:
+
+# towel, sunblock, sunscreen, hat, sunglasses, swimsuit, iPhone, homekeys, wallet, ehternet cable, gaming laptop, camera, gameboy-color, batteries, water battle, time capsule, marshmallows, beer, wine, snacks, burguers.
+
+camping_stuff = "towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"
+
+print(camping_stuff)
+
+# PYTHON LIST
+
+camping_list = ["towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"]
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
+# ME = "Coffee"
+# You = "Marshmallows"
+# Ordered
+
+me = camping_stuff[4]
+
+you = camping_stuff[-1]
+
+print(me)
+print(you)
+
+print(me + "\n" + you)
+
