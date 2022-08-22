898ac8cbaf3ad5c2ec0ba155703da74b992e85e7
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 16:45:38 +0200

    1.14.0

--- 13 ReviewMaths.py
+++ 13 ReviewMaths.py
@@ -14,30 +14,3 @@
 # On python to find the smallest number of two numbers: `min(a, b)`
 # On python to find the largest number of three numbers: `max(a, b, c)`
 # On python to find the smallest number of three numbers: `min(a, b, c)`
-
-# So lets do some basic maths in python.
-
-#Let's build robot barista!!!
-
-print("Hello, and welcome to the ITAMEASAN Cofe shop!!!!!!")
-
-# print("Waht is your name")
-
-name = input("Waht is your name? \n")
-
-print("Hello " + name+"," + " Thank you so much for coming to our shop. \n\n")
-
-print("""We have the following drinks:
-Late $2.99
-Hot $1.99
-Cold $3.99\n\n""")
-
-coffee = input("What do you wish for this sunny day? \n\n")
-
-print("Okay please, wait a second, I will take your " + coffee + " coffe inmediatly.\n")
-
-price = 2.99
-
-print("The price of your " + coffee + " is " + str(price) + " dollars.")
-
- 
