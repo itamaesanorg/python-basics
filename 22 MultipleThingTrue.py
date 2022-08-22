4a74f7c39250fde376c6782e2af4506052c8e2ee
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 18:48:05 +0200

    1.22.0

--- 22 MultipleThingTrue.py
+++ 22 MultipleThingTrue.py
@@ -2,12 +2,16 @@
 name = input("What is your name\n")
 # Great the customer with their name and thank them for coming in today using concatenation.
 
-if name == "BEN" or "ben" or "Ben" or "bEn" or "beN" or "BeN" or "BEn" or "bEN":
+if name == "Ben":
     evil_status = input("\n Are you evil?\n")
-    if evil_status == "Y" or "y" or "Ye" or "ye" or "YES" or "Yes" or "yes" or "Of course" or "of course" or "ofcourse" or "Ofcourse":
+    if evil_status == "yes":
         print("You're not welcome here Ben! Get out of here!")
         exit()
     else:
         print("Oh! You are one of those good Bens! Come in! \n\n\n")
+    if 2 < 3:
+        print("2 is less than 3")
+        if 1 < 4:
+            print("1 is less than 4")
 else:
     print("Hello " + name + ", welcome to the coffee shop. \n\n\n")
