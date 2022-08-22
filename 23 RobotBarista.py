fadd1a821676b072a00569cddf5de0221f692162
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 18:54:24 +0200

    1.23.0

---
+++ 23 RobotBarista.py
@@ -0,0 +1,15 @@
+# Coffee menu
+menu = "Black Coffe, Espresso, Latte, Capuccino, Frappuccino"
+
+# Ask the customer what they want to order, and store it in a variable called order.
+order = input(name + ", what would you like to order? Here is what we serve: " + menu + "\n")
+
+# Ask the customer how many of the order they want, and store it in a variable called quantity.
+quantity = input("How many do you wish for? \n")
+
+
+# Calculate the total price of the order, and store it in a variable called total.
+if order == "Frappuccino":
+    price = 13
+else:
+    price = 8
