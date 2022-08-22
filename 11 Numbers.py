a7820fe1b1a967e0833828fef1b1be5bd9126d9d
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 16:39:58 +0200

    1.13.0

--- 11 Numbers.py
+++ 11 Numbers.py
@@ -6,4 +6,4 @@ age = 31
 
 actual_age = 31.96 # This is a floating point number.
 
-print(5 + 7)
+print(5 ** -7 + 6 / 12 -4 ** 4 - 12)
---
+++ 12 Equations.py
@@ -0,0 +1,15 @@
+# Let's work with the equations.
+
+name = "Mike"
+
+age = 31
+
+actual_age = 31.96
+
+math = 5 ** 7 + 6 / 9 * 6 - 4
+
+
+results = age + actual_age + math
+
+
+print(results)
