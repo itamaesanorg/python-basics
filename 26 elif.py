70d123ba48725caed633e9c20231675291671748
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 19:27:31 +0200

    1.26.0 elif, yes elif LOL

---
+++ 26 elif.py
@@ -0,0 +1,23 @@
+# Here we will use a while loop to keep asking the customer for the quantity of the order until they enter a valid number.
+
+# Coffee menu
+menu = "Black Coffe, Espresso, Latte, Capuccino, Frappuccino\n"
+
+name = input("What is your name? \n")
+
+# Ask the customer what they want to order, and store it in a variable called order.
+order = input(name + ", what would you like to order? Here is what we serve: " + menu + "\n")
+
+# Calculate the total price of the order, and store it in a variable called total.
+    
+    
+if order == "Frappuccino":
+    price = 13
+    ilef order 
+
+
+
+
+
+
+print(price)
