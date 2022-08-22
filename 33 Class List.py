a1efa7c8565397da37d976242e9974d6456bd89d
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 20:55:30 +0200

    1.33.0 Class List is here!!!!!

---
+++ 33 Class List.py
@@ -0,0 +1,13 @@
+# Hey we are going on a trip to the Camp Beach.
+
+# Here are some things you will need to carry with you:
+
+# towel, sunblock, sunscreen, hat, sunglasses, swimsuit, iPhone, homekeys, wallet, ehternet cable, gaming laptop, camera, gameboy-color, batteries, water battle, time capsule, marshmallows, beer, wine, snacks, burguers.
+
+camping_stuff = "towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"
+print(camping_stuff)
+
+# python list
+
+camping_list = [ "towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers" ]
+
