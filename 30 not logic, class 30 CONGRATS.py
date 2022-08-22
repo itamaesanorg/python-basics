34ffefe994a69f32344c85447212b3e1e5df2404
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 20:38:20 +0200

    1.30.0

--- 30 not logic, class 30 CONGRATS.py
+++
@@ -1,6 +0,0 @@
-print("You enter into a bar, the bartender asks you what is your name?\n")
-# I do this to know if you read or just copy paste, if you read this, to run the code, remove the emoji from the file name.
-
-
-name = input("What is your name?\n")
-
