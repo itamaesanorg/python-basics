4b307b79b7d40367ac9d99ec7fe93f25e6cf5f2d
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 18:10:33 +0200

    1.20.0

--- 20 IF IF IF IF ELSE.py
+++ 20 IF IF IF IF ELSE.py
@@ -1,2 +1,24 @@
 # Lets play with if if if if else!!!
 
+print("\n The Bar was dark and a voice asked from the back of the room: \n \n Ben, is that you?")
+
+name = input("\n")
+menu = "Black Coffe, Espresso, Latte, Capuccino"
+
+
+if name == "yes":
+    print("Are you evil?")
+    evil = input("\n")
+    if evil == "yes":
+        print("Get out of here!")
+    else:
+        print("You are not evil!")
+        print("Hello " + name + ", welcome to the coffee shop. \n")
+        print("We have the following drinks: " + menu)
+        order = input()
+        price = 8
+        quantity = input("How many do you wish for? \n")
+        total = price * int(quantity)
+        print(total)
+        print("Thank your total is $" + str(total))
+        print("Sounds great " + name + ", we'll have your " + quantity + " " + order + " right out for you.")
