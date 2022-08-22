2697430074885657b96f983b62c4d8e9e827373a
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 20:44:51 +0200

    1.31.0 not logic

--- 30 not + booleans, class 30 CONGRATS.py
+++
@@ -1,9 +0,0 @@
-print("You enter into a bar, the bartender asks you what is your name?")
-# I do this to know if you read or just copy paste, if you read this, to run the code, remove the emoji from the file name.
-
-name = input("\n")
-
-if not name == "Ben":
-    print("Get out of here!")
-else:
-    print("Hello " + name + ", welcome to the coffee shop. \n")
---
+++ 31 not logic.py
@@ -0,0 +1,22 @@
+# Open terminal in Python 3 and type:
+# >>> me = "Mike"
+# >>> me
+# 'Mike'
+# >>> me == "Mike"
+# True
+# >>> print(type(me == "Mike"))
+# <class 'bool'>
+# >>> print(type(me == "Mike"))
+# <class 'bool'>
+# >>> not me == "Nick" 
+# True
+
+print("You enter into a bar, the bartender asks you what is your name?")
+# I do this to know if you read or just copy paste, if you read this, to run the code, remove the emoji from the file name.
+
+name = input("\n")
+
+if not name != "Ben":
+    print("Get out of here!")
+else:
+    print("Hello " + name + ", welcome to the coffee shop. \n")
