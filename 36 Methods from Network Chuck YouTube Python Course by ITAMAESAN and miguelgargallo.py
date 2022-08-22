18e02c12af0a6ad3c1434fc6743f720dc0b8bb7b
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 22:13:51 +0200

    Network Chuck YouTube Python Course by ITAMAESAN

--- 36 Methods from Network Chuck YouTube Python Course by ITAMAESAN and miguelgargallo.py
+++ 36 Methods from Network Chuck YouTube Python Course by ITAMAESAN and miguelgargallo.py
@@ -1,13 +1,21 @@
+# THIS IS IN PROGRESS https://www.youtube.com/watch?v=rW5sCgSSpI8
+
 # Let's go with Methods!
 # Methods are functions that belong to a class, in Python we use the dot operator to access them.
 # Methods in Python are defined with the def keyword and also have a return statement, but they are not required to have a return statement, cause they can return None, but also they can return any value.
-
-supplies = ["towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"]
-
-camp_site = ["Crystal Lake", 404, 89.3, True]
-
-supplies.append("toilet paper")
-
-print(supplies)
-
 # By append we add an item to the end of the list.
+
+# supplies = ["towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"]
+
+# camp_site = ["Crystal Lake", 404, 89.3, True]
+
+# supplies.append("toilet paper")
+
+# Method 1
+# supplies.append("bidet")
+
+# print(supplies)
+
+# Method 2
+
+# append.extend(["toilet paper", "bidet"])
