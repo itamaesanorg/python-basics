4236a619b3bd09742d94e835845e3e62c56048b4
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 18:44:26 +0200

    1.23.0

--- 23 Ben is good.py
+++ 23 Ben is good.py
@@ -2,9 +2,9 @@
 name = input("What is your name\n")
 # Great the customer with their name and thank them for coming in today using concatenation.
 
-if name == "BEN" or "ben" or "Ben" or "bEn" or "beN" or "BeN" or "BEn" or "bEN":
+if name == "Ben":
     evil_status = input("\n Are you evil?\n")
-    if evil_status == "Y" or "y" or "Ye" or "ye" or "YES" or "Yes" or "yes" or "Of course" or "of course" or "ofcourse" or "Ofcourse":
+    if evil_status == "yes":
         print("You're not welcome here Ben! Get out of here!")
         exit()
     else:
