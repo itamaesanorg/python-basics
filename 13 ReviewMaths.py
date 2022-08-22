0f24519983de79ed8c8924e2dc78f8302e8067b6
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 16:43:44 +0200

    1.13.1

---
+++ 13 ReviewMaths.py
@@ -0,0 +1,43 @@
+# Lets review some maths
+
+# On python to:
+
+# On python to sum two numbers: `a + b`
+# On python to multiply two numbers: `a * b`
+# On python to divide two numbers: `a / b`
+# On python to subtract two numbers: `a - b`
+# On python to find the remainder of two numbers: `a % b`
+# On python to find the power of two numbers: `a ** b`
+# On python to find the square root of a number: `sqrt(a)`
+# On python to find the absolute value of a number: `abs(a)`
+# On python to find the largest number of two numbers: `max(a, b)`
+# On python to find the smallest number of two numbers: `min(a, b)`
+# On python to find the largest number of three numbers: `max(a, b, c)`
+# On python to find the smallest number of three numbers: `min(a, b, c)`
+
+# So lets do some basic maths in python.
+
+#Let's build robot barista!!!
+
+print("Hello, and welcome to the ITAMEASAN Cofe shop!!!!!!")
+
+# print("Waht is your name")
+
+name = input("Waht is your name? \n")
+
+print("Hello " + name+"," + " Thank you so much for coming to our shop. \n\n")
+
+print("""We have the following drinks:
+Late $2.99
+Hot $1.99
+Cold $3.99\n\n""")
+
+coffee = input("What do you wish for this sunny day? \n\n")
+
+print("Okay please, wait a second, I will take your " + coffee + " coffe inmediatly.\n")
+
+price = 2.99
+
+print("The price of your " + coffee + " is " + str(price) + " dollars.")
+
+ 
