e4731ae62ebd750893634c56228a486f6b39f3a0
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 21:41:51 +0200

    Network Chuck YouTube Python Course by ITAMAESAN

---
+++ 36 Methods from Network Chuck YouTube Python Course by ITAMAESAN and miguelgargallo.py
@@ -0,0 +1,8 @@
+# Let's go with Methods!
+# Methods are functions that belong to a class, in Python we use the dot operator to access them.
+# Methods in Python are defined with the def keyword and also have a return statement, but they are not required to have a return statement, cause they can return None, but also they can return any value.
+
+camping_list = ["towel", "sunblock", "sunscreen", "hat", "sunglasses", "swimsuit", "iPhone", "homekeys", "wallet", "ehternet cable", "gaming laptop", "camera", "gameboy-color", "batteries", "water battle", "time capsule", "marshmallows", "beer", "wine", "snacks", "burguers"]
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
