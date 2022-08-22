4cc2d3c81415973b0b1e696b7b650451d522939f
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 16:30:21 +0200

    1.0.10

---
+++ 10 Floating.py
@@ -0,0 +1,19 @@
+# Lets do some type functions but now with decimals too.
+
+# We will get at this point 3 types of characters: strings, integers, and floats.
+
+name = "Mike"
+
+age = 31
+
+actual_age = 31.96 # This is a floating point number.
+
+print(name)
+print(age)
+
+print(type(name))
+
+print(type(age))
+
+print(type(actual_age))
+
