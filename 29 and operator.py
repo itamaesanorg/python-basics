da9beb43ca26b91a91fb1c7f12fddb6234a417b0
miguelgargallo <mgargallo16@gmail.com>
2022-08-22 20:22:17 +0200

    1.29.0 and operator

--- 29 and operator.py
+++ 29 and operator.py
@@ -7,15 +7,11 @@ name = input("What is your name?\n")
 
 if name == "Ben" or name == "Patricia" or name == "Loki":
     evil_status = input("Are you evil?\n")
-    if evil_status == "yes":
+    good_deeds = int(input("How many good things you did today? " + name + "?\n"))
+    if evil_status == "yes" and good_deeds < 4:
         print("You're not welcome here " + name + " Get out of here!")
         exit()
     else:
-        print("How many good things you did today?" + name + "?")
-        ans = input("")
-        if ans > "4" or ans == "4":
-            print("Okay, come in!")
-        else:
-            print("You are not welcome here " + name + "! Get out of here!")
+        print("Okay, come in!\n")
 else:
     print("Hello " + name + ", welcome to the coffee shop. \n\n\n")
